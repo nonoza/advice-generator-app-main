073cc8254dd2df4692d133c107f5fd5177a6e9b7
Pretty Kunene <pretty.kunene@wundermanthompson.com>
2022-09-13 17:47:50 +0200

    modifing my project

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
+//Variables
 let getRandomID = '';
 let getRandomAdvice = '';
 let getadvicebtn = document.getElementById('advice-btn');
 
+
+//Load the function when the page loads
+window.addEventListener('load', () =>{
+    setTimeout(()=> { 
+    getAdvicegenerator();
+}, 1000);
+});
+
+//When the button is clicked the event get the advice and id from the function
+
 getadvicebtn.addEventListener("click",getAdvicegenerator);
 
-function getAdvicegenerator(){
+//Function to get the advice and id from an API using JSON and Get method.
+
+ function getAdvicegenerator(){
+    setTimeout(()=> { 
     $ .ajax ({
         url :"https://api.adviceslip.com/advice?method=getAdvice",
         method : "GET",
         dataType : "json",
-
+        
         success : function (request){
             getRandomID = request.slip.id;//Getting ID from the URL
             getRandomAdvice = request.slip.advice;//requesting the advice from the url.
@@ -17,10 +31,14 @@ function getAdvicegenerator(){
             $('#advice-id').html(getRandomID);
             $('#advice-text').html('"'+ getRandomAdvice +'"');
         },
+        
         error: function(xhr, status, error){
             $('#advice-id').html("ID not valid");
             $('#advice-text').html("text not valid");
         }
+    }, 1000);
     });
+    
 }
+
 
